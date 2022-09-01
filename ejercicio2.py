22989944f482bf65f7b04c529d2dfa20f2ace29d
Alejandra Rivera <alejandra.rivera@frubana.com>
2022-09-01 13:16:31 -0500

    agregar ejercicio 2 y modificaciones

--- ejercicio2.py
+++ ejercicio2.py
@@ -3,9 +3,9 @@ import pandas as pd
 def main():
 	#retrieve data
 	data = pd.read_excel("Data_For_Test_2022_08_31.xlsx")
-	# 1. filter report 
-	data = data[data['vehiculo']=='Carry']
-	print(data.value_counts(data['vehiculo']=='Carry'))
+	# 1. filter report before 20h
+	data = data[data['Ciudad']=='Bogotá']
+	print(data.value_counts(data['Ciudad']=='Bogotá'))
 
 
 if __name__ == '__main__':
