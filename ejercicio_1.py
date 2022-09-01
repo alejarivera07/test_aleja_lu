ea059c5316131df007bcd54015ce3236f22e8398
Alejandra Rivera <alejandra.rivera@frubana.com>
2022-09-01 13:05:58 -0500

    cambio de función

--- ejercicio_1.py
+++ ejercicio_1.py
@@ -2,10 +2,10 @@ import pandas as pd
 
 def main():
 	#retrieve data
-	data = pd.read_csv("Data_For_Test_2022_08_31.csv")
+	data = pd.read_excel("Data_For_Test_2022_08_31.csv")
 	# 1. filter report before 20h
 	data = data[data['Hora']<20]
-	print(data.value_counts(data['Hora']))
+	print(data.value_counts(data['Hora']<20))
 
 
 if __name__ == '__main__':
