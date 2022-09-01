485dabcd6e5ed8d4ef96809ef4c13e333d22f05e
Alejandra Rivera <alejandra.rivera@frubana.com>
2022-09-01 13:38:55 -0500

    arreglos minimos

--- ejercicio4.py
+++ ejercicio4.py
@@ -4,8 +4,9 @@ def main():
 	#retrieve data
 	data = pd.read_excel("Data_For_Test_2022_08_31.xlsx")
 	# 1. filter report before 20h
-	data = data[data['vehiculo']=='Carry']
-	print(data.value_counts(data['Hora']=='Carry'))
+	data = data[data['puntos']>5]
+	print(data.value_counts(data['puntos']>5))
+
 
 if __name__ == '__main__':
 	main()
