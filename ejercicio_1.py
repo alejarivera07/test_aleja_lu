9594caadd69d284181f2ccaa8bc636d06d343063
Alejandra Rivera <alejandra.rivera@frubana.com>
2022-09-01 13:07:05 -0500

    cambiamos extención

--- ejercicio_1.py
+++ ejercicio_1.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 def main():
 	#retrieve data
-	data = pd.read_excel("Data_For_Test_2022_08_31.csv")
+	data = pd.read_excel("Data_For_Test_2022_08_31.xlsx")
 	# 1. filter report before 20h
 	data = data[data['Hora']<20]
 	print(data.value_counts(data['Hora']<20))
